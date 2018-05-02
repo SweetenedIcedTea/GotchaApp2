bbeb4445368d9b4649cf27f956a44c58a090643a
SweetenedIcedTea <ethanzhang08@yahoo.com>
2018-05-02 13:09:39 -0400

    Added New Views
    Added RegisterView and Login View

---
+++ GotchaApp/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  GotchaApp
+//
+//  Created by Ethan Zhang on 5/2/18.
+//  Copyright © 2018 NepinNep. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ GotchaApp/RegisterViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RegisterViewController.swift
+//  GotchaApp
+//
+//  Created by Ethan Zhang on 5/2/18.
+//  Copyright © 2018 NepinNep. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
